456ba1b241de6bda195b1f81458bb3e1e6426186
DoodleBin <alexahatcher4@gmail.com>
2025-12-07 12:06:33 -0600

    Added in all our methods

--- project5.cpp
+++ project5.cpp
@@ -37,6 +37,30 @@ vector<int> ED_DP(string &src, string &dest, bool useASM) {
     // Setup the table, size (n+1)x(m+1), filled with -1s.
     vector<int> table( (n+1)*(m+1), -1 );
 
+    // Base case
+    for(int i = 0; i < n+1; i++){
+        table[i*numCols + 0] = i;
+    }
+
+    // Base case
+    for(int j = 0; j < numCols; j++){
+        table[0*numCols + j] = j;
+    }
+
+    for(int row = 1; row < n+1; row++){
+        for(int col = 1; col < numCols; col++){
+            int curr = row * numCols + col;
+            if(src[row] == dest[col]){
+                table[curr] = 1 + table[(row-1) * numCols + col - 1];
+            }else{
+                int del = table[(row-1) * numCols + col];
+                int ins = table[row * numCols + col - 1];
+                int sub = table[(row-1) * numCols + col - 1];
+                table[curr] = 1 + min({del, ins, sub});
+            }
+        }
+    }
+
     // Return the completed table.
     return table;
 }
@@ -52,7 +76,9 @@ vector<int> ED_Mem(string &src, string &dest, bool useASM) {
 
     // Setup the table, size (n+1)x(m+1), filled with -1s.
     vector<int> table( (n+1)*(m+1), -1 );
-
+    int i = n;
+    int j = m;
+    int minCost = ED_Mem_helper(src, dest, useASM, numCols, i, j, table);
     // Return the completed table.
     return table;
 }
@@ -61,17 +87,31 @@ vector<int> ED_Mem(string &src, string &dest, bool useASM) {
  * ED_Mem_helper - recursive helper function for memoization.
  *                 Note: numCols input for ease of indexing.
  */
-int ED_Mem_helper(string &src, string &dest, bool useASM, int numCols,
-                  int i, int j, vector<int> &table) {
-    return -1;
+int ED_Mem_helper(string &src, string &dest, bool useASM, int numCols, int i, int j, vector<int> &table) {
+    int curr = i * numCols + j;
+
+    if ((i == 0) || (j == 0)){
+        table[curr] = 0;
+        return max(i, j);
+    }
+    else if(src[i] == dest[j]){
+        table[curr] = ED_Mem_helper(src, dest, useASM, numCols, i-1, j-1, table);
+    }
+    else{
+        int del =  ED_Mem_helper(src, dest, useASM, numCols, i-1, j, table);
+        int ins =  ED_Mem_helper(src, dest, useASM, numCols, i, j-1, table);
+        int sub =  ED_Mem_helper(src, dest, useASM, numCols, i-1, j-1, table);
+        table[curr] = 1 + min({del, ins, sub});
+    }
+
+    return table[curr];
 }
 
 /*
  * getNumEdits - given a filled table, return the min number of edits.
  */
 int getNumEdits(string &src, string &dest, vector<int> &table) {
-    int numEdits;
-    return numEdits;
+    return getEdits(src, dest, table).size();
 }
 
 /*
@@ -79,6 +119,57 @@ int getNumEdits(string &src, string &dest, vector<int> &table) {
  */
 vector<Edit> getEdits(string &src, string &dest, vector<int> &table) {
     vector<Edit> edits;
+    int numCols = dest.length() +1;
+
+    int i = src.length() +1;
+    int j = dest.length() +1;
+
+    while((i != 0) && (j != 0)){
+        int curr = i * numCols + j;
+
+        if(i == 0){ //add remaining letters of dest
+            while(j > 0){
+                Edit firstEdit = Edit("ins", src[i], dest[j], i);
+                edits.push_back(firstEdit);
+                j--;
+            }
+        }
+
+        else if(j == 0){ //delete preceding letters of src
+            while(i > 0){
+                Edit firstEdit = Edit("del", src[i], dest[j], i);
+                edits.push_back(firstEdit);
+                i--;
+            }
+        }
+
+        else if(src[i] == dest[j]){
+            Edit firstEdit = Edit("match", src[i], dest[j], i);
+            edits.push_back(firstEdit);
+            i--;
+            j--;
+        }else{
+            int left = table[i*numCols + j - 1];
+            int up = table[(i-1)*numCols + j];
+            int diag = table[(i-1)*numCols + j - 1];
+            int minimum = min({left, up, diag});
+            if(left == minimum){
+                Edit newEdit = Edit("ins", src[i], dest[j], i);
+                j--;
+                edits.push_back(newEdit);
+            } else if(up == minimum){
+                Edit newEdit = Edit("del", src[i], dest[j], i);
+                i--;
+                edits.push_back(newEdit);
+            }else{
+                Edit newEdit = Edit("sub", src[i], dest[j], i);
+                i--;
+                j--;
+                edits.push_back(newEdit);
+            }
+        }
+    }
+
     return edits;
 }
 
